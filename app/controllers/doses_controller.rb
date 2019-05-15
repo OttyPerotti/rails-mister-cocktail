9a211f599dc5aef3c686726c9c6f64fbc87495ab
Ottavia Perotti <perottiottavia@gmail.com>
2019-05-15 14:43:12 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,12 +1,15 @@
 class DosesController < ApplicationController
   def new
     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_path(@dose[:cocktail_id])
+      redirect_to cocktail_path(@dose.cocktail)
     else
       render :new
     end
@@ -15,11 +18,12 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:create, :show, :new, :index] do
-    resources :doses, only: [:new, :create, :destroy]
+  resources :cocktails, only: [:create, :show, :new, :index, :destroy] do
+    resources :doses, only: [:new, :create]
   end
+  resources :doses, only: [:destroy]
 end
